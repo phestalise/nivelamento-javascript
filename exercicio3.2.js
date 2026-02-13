f187857ccc69997a7b22ef07406efe9cf9acb81f
phestalise <phestalise@gmail.com>
2026-02-12 22:23:12 -0300

    Exercício 3.2 - Propriedade inexistente e adicionada em objeto

---
+++ exercicio3.2.js
@@ -0,0 +1,10 @@
+let aluno = { nome: "Carlos", matricula: 1234 };
+
+console.log(aluno.nome);
+console.log(aluno.matricula);
+
+console.log(aluno.nota);
+
+aluno.nota = 8.5;
+
+console.log(aluno.nota);
