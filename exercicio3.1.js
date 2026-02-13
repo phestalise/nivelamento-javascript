a8c9a92273531e28baab69f9cc4f7a183bc969cc
phestalise <phestalise@gmail.com>
2026-02-12 22:12:28 -0300

    Exercício 3.1 - Uso de null e undefined em sistema de delivery

---
+++ exercicio3.1.js
@@ -0,0 +1,11 @@
+let enderecoEntrega = null;
+let numeroPedido;
+
+console.log(enderecoEntrega);
+console.log(numeroPedido);
+
+enderecoEntrega = "Rua das Flores, 123";
+numeroPedido = 4567;
+
+console.log(enderecoEntrega);
+console.log(numeroPedido);
