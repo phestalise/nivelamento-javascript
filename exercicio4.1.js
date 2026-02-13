447c98c6d7d0102cfa2b71373d236514230c7d3e
phestalise <phestalise@gmail.com>
2026-02-12 22:28:06 -0300

    Exercício 4.1 - Manipulação de array com length e push

---
+++ exercicio4.1.js
@@ -0,0 +1,10 @@
+let tarefas = ["Estudar JavaScript", "Treinar", "Ler livro"];
+
+console.log(tarefas[0]);
+console.log(tarefas[2]);
+console.log(tarefas.length);
+
+tarefas.push("Revisar conteúdo");
+
+console.log(tarefas.length);
+console.log(tarefas[3]);
