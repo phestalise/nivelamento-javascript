44c888b8471f4366cb1df65dec0ce67fd9394be5
phestalise <phestalise@gmail.com>
2026-02-12 22:08:46 -0300

    Exercício 2.2 - Verificação de tipos com typeof

---
+++ exercicio2.2.js
@@ -0,0 +1,16 @@
+let nome = "Paulo";
+let idade = 18;
+let estudante = true;
+let endereco;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(idade);
+console.log(typeof idade);
+
+console.log(estudante);
+console.log(typeof estudante);
+
+console.log(endereco);
+console.log(typeof endereco);
