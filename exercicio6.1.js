fdbc2f3864523f388d36a560e277c4509c2af104
phestalise <phestalise@gmail.com>
2026-02-12 22:44:07 -0300

    Exercício 6.1 - Uso de for com números pares e array

---
+++ exercicio6.1.js
@@ -0,0 +1,11 @@
+// Situação A
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+
+// Situação B
+let alunos = ["Ana", "Bruno", "Clara", "Diego"];
+
+for (let i = 0; i < alunos.length; i++) {
+  console.log(i + ": " + alunos[i]);
+}
