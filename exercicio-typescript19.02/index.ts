422ca2eeebbffbb0f25bd429319665f8cddc209a
phestalise <phestalise@gmail.com>
2026-02-19 22:22:29 -0300

    criando os arrays de produtos

--- exercicio-typescript19.02/index.ts
+++ exercicio-typescript19.02/index.ts
@@ -5,3 +5,26 @@ type Produto = {
   categoria: string;
   emEstoque: boolean;
 };
+const produtos: Produto[] = [
+  {
+    id: 1,
+    nome: "iPhone 15",
+    preco: 5999.0,
+    categoria: "Eletrônicos",
+    emEstoque: true,
+  },
+  {
+    id: 2,
+    nome: "Cabo HDMI",
+    preco: 39.9,
+    categoria: "Eletrônicos",
+    emEstoque: false,
+  },
+  {
+    id: 3,
+    nome: "Tênis Running Pro",
+    preco: 349.9,
+    categoria: "Esportes",
+    emEstoque: true,
+  },
+];
